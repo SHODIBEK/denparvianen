20ca20fdd3cec898b4947208bf2cac13cdf39941
mr.agape <32167063+SHODIBEK@users.noreply.github.com>
2022-11-24 17:54:47 +0500

    ////

--- src/js/components/validation.js
+++ src/js/components/validation.js
@@ -1,42 +1,44 @@
-$(".js-validation").each(function() {
-  $(this).validate({
-    errorElement: "span",
-    rules: {
-      name: {
-        required: true
-      },
-      email: {
-        required: true,
-        email: true
-      },
-      message: {
-        required: true
-      },
-      agree: {
-        required: true
-      }
-    },
-    messages: {
-      name: "Пожалуйста, укажите ваше имя",
-      email: {
-        required: "Нам нужен ваш адрес электронной почты, чтобы связаться с вами",
-        email: "Ваш адрес электронной почты должен быть в формате name@domen.com"
-      },
-      message: 'Пожалуйста, напишите сообщение',
-      agree: 'Примите политику конфиденциальности'
-    },
-    submitHandler: function(form) {
-      var serializedData = $(form).serialize();
+$(".js-validation").each(function () {
+	$(this).validate({
+		errorElement: "span",
+		rules: {
+			name: {
+				required: true,
+			},
+			email: {
+				required: true,
+				email: true,
+			},
+			message: {
+				required: true,
+			},
+			agree: {
+				required: true,
+			},
+		},
+		messages: {
+			name: "Пожалуйста, укажите ваше имя",
+			email: {
+				required:
+					"Нам нужен ваш адрес электронной почты, чтобы связаться с вами",
+				email: "Ваш адрес электронной почты должен быть в формате name@domen.com",
+			},
+			message: "Пожалуйста, напишите сообщение",
+			agree: "Примите политику конфиденциальности",
+		},
+		submitHandler: function (form) {
+			const formData = new FormData(form);
 
-      $.ajax({
-          url: "send.php", 
-          type: "POST",             
-          data: serializedData,
-          success: function(data) {
-            $('.modal--success').fadeIn();
-          }
-      });
-      return false;
-    },
-  });
+			fetch("send.php", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded",
+				},
+				body: new URLSearchParams(formData).toString(),
+			})
+				.then(() => $(".modal--success").fadeIn())
+				.catch((error) => alert(error));
+			return false;
+		},
+	});
 });
